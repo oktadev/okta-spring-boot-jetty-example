3fbbc42d49a0ccda7fe3c6040112d08ee7a5c8b4
Matt Raible <matt.raible@okta.com>
2019-10-29 16:56:45 -0600

    Updates after QA

--- jettyembedded/src/main/java/com/okta/jettyembedded/HikesTodoServlet.java
+++ jettyembedded/src/main/java/com/okta/jettyembedded/HikesTodoServlet.java
@@ -1,29 +1,30 @@
 package com.okta.jettyembedded;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @WebServlet(name = "HikesTodoServlet", urlPatterns = {"hikes"}, loadOnStartup = 1)
 public class HikesTodoServlet extends HttpServlet {
 
     // Not synchronized
-    ArrayList<String> hikes = new ArrayList<String>(Arrays.asList(
-        "Wonderland Trail", "South Maroon Peak", "Tour du Mont Blanc", "Teton Crest Trail", "Everest Base Camp via Cho La Pass", "Kesugi Ridge"
+    private List<String> hikes = new ArrayList<>(Arrays.asList(
+            "Wonderland Trail", "South Maroon Peak", "Tour du Mont Blanc",
+            "Teton Crest Trail", "Everest Base Camp via Cho La Pass", "Kesugi Ridge"
     ));
-    
+
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+        throws IOException {
         response.getWriter().print(String.join("\n", this.hikes));
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+        throws IOException {
         String hike = request.getParameter("hike");
         if (hike == null) {
             response.setStatus(400);
@@ -40,7 +41,7 @@ public class HikesTodoServlet extends HttpServlet {
     }
 
     protected void doDelete(HttpServletRequest request, HttpServletResponse response)
-        throws ServletException, IOException {
+        throws IOException {
         String hike = request.getParameter("hike");
         if (hike == null) {
             response.setStatus(400);
@@ -51,5 +52,6 @@ public class HikesTodoServlet extends HttpServlet {
             response.getWriter().print(String.join("\n", this.hikes));
         }
     }
-    
+
 }
+
